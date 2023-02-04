724b1296b0aa3bf90a1222afa094acfd2fe44034
Mehmet Kabasakal <sehriistanbul@gmail.com>
2023-02-04 11:59:09 +0300

    brand and firm added

--- stock/serializers.py
+++ stock/serializers.py
@@ -11,12 +11,24 @@ class CategorySerializer(serializers.ModelSerializer):
     def get_product_count(self, obj):
         return Product.objects.filter(category_id=obj.id).count()
     
-
 class ProductSerializer(serializers.ModelSerializer):
     
+    category = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = (
+            "id",
+            "name",
+            "category",
+            "category_id",
+            "brand",
+            "brand_id",
+            "stock",
+        )
+
+        read_only_fields = ("stock",)        
 
 class CategoryProductSerializer(serializers.ModelSerializer):
     
@@ -29,3 +41,24 @@ class CategoryProductSerializer(serializers.ModelSerializer):
         
     def get_product_count(self, obj):
         return Product.objects.filter(category_id=obj.id).count()
+    
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = (
+            'id',
+            'name',
+            'image'
+        )
+        
+class FirmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Firm
+        fields = (
+            'id',
+            'name',
+            'phone',
+            'image',
+            'address'
+        )
+        
